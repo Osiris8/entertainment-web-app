d85695962c9380241232e3ba609b1f26da797e7f
Osiris8 <miganosiris8@gmaaail.com>
2023-06-25 00:08:43 +0200

    update Navigation.js in column

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,25 @@
 function Navigation() {
   return (
-    <div className=" flex flex-direction-column nav-bg-color margin-16 padding-16">
-      <ul class="">
-        <li>
-          <i class="fa fa-film" aria-hidden="true"></i>
-        </li>
-        <li>
-          <i class="fa fa-home" aria-hidden="true"></i>
-        </li>
-        <li>
-          <i class="fa fa-heart" aria-hidden="true"></i>
-        </li>
-        <li>
-          <i class="fa fa-arrow-down" aria-hidden="true"></i>
-        </li>
-        <li>
-          <i class="fa fa-arrow-up" aria-hidden="true"></i>
-        </li>
-      </ul>
+    <div className="col-2">
+      <div className=" flex flex-direction-column nav-bg-color margin-16 padding-16">
+        <ul class="">
+          <li>
+            <i class="fa fa-film" aria-hidden="true"></i>
+          </li>
+          <li>
+            <i class="fa fa-home" aria-hidden="true"></i>
+          </li>
+          <li>
+            <i class="fa fa-heart" aria-hidden="true"></i>
+          </li>
+          <li>
+            <i class="fa fa-arrow-down" aria-hidden="true"></i>
+          </li>
+          <li>
+            <i class="fa fa-arrow-up" aria-hidden="true"></i>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
