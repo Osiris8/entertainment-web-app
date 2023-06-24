a42f245d85e0158d9add6773b01b3bd237acd6f3
Osiris8 <miganosiris8@gmaaail.com>
2023-06-25 00:12:52 +0200

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles/App.css";
 import "./styles/Flexbox.css";
-import Navigation from "./Components/Navigation";
-import Movie from "./Components/Movie";
+import Navigation from "./components/Navigation";
+import Movie from "./components/Movie";
 
 function App() {
   return (
