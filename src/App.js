c4ea1d40433299c17c8047be2facd8360b2aaf2f
Osiris8 <miganosiris8@gmaaail.com>
2023-06-26 19:12:16 +0200

    delete Movie component import in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./styles/App.css";
 import "./styles/Flexbox.css";
 import Navigation from "./components/Navigation";
-import Movie from "./components/Movie";
 
 function App() {
   return (
     <div className="container">
       <div className="row">
         <Navigation />
-        <Movie />
       </div>
     </div>
   );
