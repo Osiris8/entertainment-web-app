5d2ec50996ccaaa923e05eb42ba754cf46d6d19d
Osiris8 <miganosiris8@gmaaail.com>
2023-06-30 21:23:08 +0200

    add search state and search movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,157 +1,33 @@
-import { useEffect, useState } from "react";
+import Card from "./Card";
+import { useState, useEffect } from "react";
 import axios from "axios";
 function Movie() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     axios
-      .get("https://restcountries.com/v3.1/all")
-      .then((res) => setData(res.data));
-  }, []);
+      .get(
+        "https://api.themoviedb.org/3/search/movie?api_key=bcfff3e4dbf56262dbedd3c5c677bcba&query=" +
+          search +
+          "&langage=fr-FR"
+      )
+      .then((res) => setData(res.data.results));
+  }, [search]);
+
   return (
     <div className="col-lg-10 padding-32">
       <input
         class="form-control"
         type="text"
         placeholder="Search for movies and TV series"
+        onChange={(e) => setSearch(e.target.value)}
       />
 
       <div className="container">
         <div className="row">
-          <div className="col-lg-3 mt-3">
-            <div class="card ">
-              <img
-                src="https://image.tmdb.org/t/p/original//fJbw16AwM59dEhSiCIAfFGgIgOP.jpg"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Avengers : Infinity Wars</h5>
-                <span>Sorti le : 25/04/2018</span>
-                <h6 className="mt-1">
-                  8/10 <i class="fa fa-star" aria-hidden="true"></i>
-                </h6>
-                <div className="flex flex-direction-row gap-genre mt-2">
-                  <span class="badge text-bg-dark">Dark</span>
-                  <span class="badge text-bg-dark">Dark</span>
-                  <span class="badge text-bg-dark">Dark</span>
-                </div>
-                <h6 className="mt-2">Synopsie</h6>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <img
-                  className="flex center heart-color"
-                  width="25"
-                  height="25"
-                  src="https://img.icons8.com/ios/50/like--v1.png"
-                  alt="like--v1"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 mt-3">
-            <div class="card ">
-              <img
-                src="https://image.tmdb.org/t/p/original//fJbw16AwM59dEhSiCIAfFGgIgOP.jpg"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Avengers : Infinity Wars</h5>
-                <span>Sorti le : 25/04/2018</span>
-                <h6 className="mt-1">
-                  8/10 <i class="fa fa-star" aria-hidden="true"></i>
-                </h6>
-                <div className="flex flex-direction-row gap-genre mt-2">
-                  <span class="badge text-bg-dark">Dark</span>
-                  <span class="badge text-bg-dark">Dark</span>
-                  <span class="badge text-bg-dark">Dark</span>
-                </div>
-                <h6 className="mt-2">Synopsie</h6>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <img
-                  className="flex center heart-color"
-                  width="25"
-                  height="25"
-                  src="https://img.icons8.com/ios/50/like--v1.png"
-                  alt="like--v1"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 mt-3">
-            <div class="card ">
-              <img
-                src="https://image.tmdb.org/t/p/original//fJbw16AwM59dEhSiCIAfFGgIgOP.jpg"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Avengers : Infinity Wars</h5>
-                <span>Sorti le : 25/04/2018</span>
-                <h6 className="mt-1">
-                  8/10 <i class="fa fa-star" aria-hidden="true"></i>
-                </h6>
-                <div className="flex flex-direction-row gap-genre mt-2">
-                  <span class="badge text-bg-dark">Dark</span>
-                  <span class="badge text-bg-dark">Dark</span>
-                  <span class="badge text-bg-dark">Dark</span>
-                </div>
-                <h6 className="mt-2">Synopsie</h6>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <img
-                  className="flex center heart-color"
-                  width="25"
-                  height="25"
-                  src="https://img.icons8.com/ios/50/like--v1.png"
-                  alt="like--v1"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 mt-3">
-            <div class="card ">
-              <img
-                src="https://image.tmdb.org/t/p/original//fJbw16AwM59dEhSiCIAfFGgIgOP.jpg"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Avengers : Infinity Wars</h5>
-                <span>Sorti le : 25/04/2018</span>
-                <h6 className="mt-1">
-                  8/10 <i class="fa fa-star" aria-hidden="true"></i>
-                </h6>
-                <div className="flex flex-direction-row gap-genre mt-2">
-                  <span class="badge text-bg-dark">Dark</span>
-                  <span class="badge text-bg-dark">Dark</span>
-                  <span class="badge text-bg-dark">Dark</span>
-                </div>
-                <h6 className="mt-2">Synopsie</h6>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <img
-                  className="flex center heart-color"
-                  width="25"
-                  height="25"
-                  src="https://img.icons8.com/ios/50/like--v1.png"
-                  alt="like--v1"
-                />
-              </div>
-            </div>
-          </div>
+          {data.map((movie) => {
+            return <Card key={movie.id} movie={movie} />;
+          })}
         </div>
       </div>
     </div>
