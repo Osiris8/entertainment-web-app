c8f3f3539044ac73569c6081264924b527777355
Osiris8 <miganosiris8@gmaaail.com>
2023-07-01 19:33:57 +0200

    update App.Js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import "./styles/App.css";
 import "./styles/Flexbox.css";
 import Movie from "./components/Movie";
 function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <Movie />
-      </div>
-    </div>
-  );
+  return <Movie />;
 }
 
 export default App;
