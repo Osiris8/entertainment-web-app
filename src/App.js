88e3b9454ed7f04cfec6324a4a7f2b16235adf3c
Osiris8 <miganosiris8@gmaaail.com>
2023-07-01 16:10:02 +0200

    delete Navigation components of App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./styles/App.css";
 import "./styles/Flexbox.css";
-import Navigation from "./components/Navigation";
 import Movie from "./components/Movie";
 function App() {
   return (
     <div className="container">
       <div className="row">
-        <Navigation />
         <Movie />
       </div>
     </div>
